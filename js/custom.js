556961a08ce0b26ed3b58f341d15f72a68dd44a9
Daniel Gamez Orozco <daalgaor.17@gmail.com>
2023-10-09 20:14:00 -0700

    add npm and node js icons

--- js/custom.js
+++ js/custom.js
@@ -37,4 +37,168 @@
     let year = date.getFullYear()
     $('#copyright_year').html(year)
 
+    var frontSkills = [
+      { titulo: "JavaScript", imagenURL: "images/skills/js.png" },
+      { titulo: "TypeScript", imagenURL: "images/skills/typescript.png" },
+      { titulo: "React Js", imagenURL: "images/skills/React-icon.svg.png" },
+      { titulo: "HTML", imagenURL: "images/skills/html-5.png" },
+      { titulo: "WordPress", imagenURL: "images/skills/wordpress.png" },
+      { titulo: "EJS", imagenURL: "images/skills/ejs.png" },
+      { titulo: "Jquery", imagenURL: "images/skills/free-jquery-8-1175153.png" },
+      { titulo: "Next js", imagenURL: "images/skills/nextjs.png" }
+    ];
+
+    var stylesSkills = [
+      { titulo: "CSS", imagenURL: "images/skills/css-3.png" },
+      { titulo: "Bootstrap", imagenURL: "images/skills/bootstrap.png" },
+      { titulo: "Antd", imagenURL: "images/skills/antd.png" },
+      { titulo: "chakra UI", imagenURL: "images/skills/chakra-ui.png" },
+    ];
+
+    var backendSkills = [
+      { titulo: "Node js", imagenURL: "images/skills/nodejs.png" },
+      { titulo: "Nest js", imagenURL: "images/skills/nestjs-icon-256x255-r03j160r.png" },
+      { titulo: "Firebase", imagenURL: "images/skills/firebase.png" },
+      { titulo: "PHP", imagenURL: "images/skills/php.png" },
+      { titulo: "Codeigniter", imagenURL: "images/skills/free-codeigniter-4-1175201.png" },
+    ];
+
+    var databaseSkills = [
+      { titulo: "Mysql", imagenURL: "images/skills/mysql.png" },
+      { titulo: "Mongo DB", imagenURL: "images/skills/free-mongodb-5-1175140.png" },
+      { titulo: "Oracle", imagenURL: "images/skills/oracle.png" },
+      { titulo: "Firebase", imagenURL: "images/skills/firebase.png" },
+    ];
+
+    var toolsSkills = [
+      { titulo: "Visual Studio Code ", imagenURL: "images/skills/visualcode.png" },
+      { titulo: "Visual Studio", imagenURL: "images/skills/Visual_Studio_Icon_2019.svg.png" },
+      { titulo: "Postman", imagenURL: "images/skills/postman.png" },
+      { titulo: "Github", imagenURL: "images/skills/github.png" },
+      { titulo: "NPM", imagenURL: "images/skills/npm.png" },
+      { titulo: "Filezilla", imagenURL: "images/skills/filezilla.png" },
+    ];
+
+    function getFrontSkills(){
+      let frontendCards = ""
+      frontSkills.forEach(skill=>{
+        frontendCards += 
+        `
+        <div class="col-3">
+          <div class="card">
+            <img
+              src=${skill.imagenURL}
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">${skill.titulo}</h5>
+            </div>
+          </div>
+        </div>
+        `
+      })
+
+      $('.frontend-skills').html(frontendCards)
+    }
+
+    function getStylesSkills(){
+      let stylesCards = ""
+      stylesSkills.forEach(skill=>{
+        stylesCards += 
+        `
+        <div class="col-3">
+          <div class="card">
+            <img
+              src=${skill.imagenURL}
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">${skill.titulo}</h5>
+            </div>
+          </div>
+        </div>
+        `
+      })
+
+      $('.styles-skills').html(stylesCards)
+    }
+
+    function getBackendSkills(){
+      let backendCards = ""
+      backendSkills.forEach(skill=>{
+        backendCards += 
+        `
+        <div class="col-3">
+          <div class="card">
+            <img
+              src=${skill.imagenURL}
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">${skill.titulo}</h5>
+            </div>
+          </div>
+        </div>
+        `
+      })
+
+      $('.backend-skills').html(backendCards)
+    }
+
+    function getDatabaseSkills(){
+      let databaseCards = ""
+      databaseSkills.forEach(skill=>{
+        databaseCards += 
+        `
+        <div class="col-3">
+          <div class="card">
+            <img
+              src=${skill.imagenURL}
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">${skill.titulo}</h5>
+            </div>
+          </div>
+        </div>
+        `
+      })
+
+      $('.database-skills').html(databaseCards)
+    }
+
+    function getToolsSkills(){
+      let toolsCards = ""
+      toolsSkills.forEach(skill=>{
+        toolsCards += 
+        `
+        <div class="col-3">
+          <div class="card">
+            <img
+              src=${skill.imagenURL}
+              class="card-img-top"
+              alt="..."
+            />
+            <div class="card-body">
+              <h5 class="card-title">${skill.titulo}</h5>
+            </div>
+          </div>
+        </div>
+        `
+      })
+
+      $('.tools-skills').html(toolsCards)
+    }
+
+    getFrontSkills()
+    getStylesSkills()
+    getBackendSkills()
+    getDatabaseSkills()
+    getToolsSkills()
+
+
 })(jQuery);
